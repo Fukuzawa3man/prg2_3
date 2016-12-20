a9a3e2b80c8e4023853cd1fdbe344a1a3434ceed
Wako Oshiro <e165711@ie.u-ryukyu.ac.jp>
2016-12-20 11:20:58 +0900

    Maiking map

--- src/Main/jp/ac/uryukyu/ie/e165711/main.java
+++ src/Main/jp/ac/uryukyu/ie/e165711/main.java
@@ -3,5 +3,46 @@ package jp.ac.uryukyu.ie.e165711;
 /**
  * Created by e165711 on 2016/12/20.
  */
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("/Users/e165711/IdeaProjects/prg2_3/src/Main/map.txt");
+            BufferedReader Map = new BufferedReader(new FileReader(file));
+            String map;
+            int i = 0;
+            while((map = Map.readLine()) != null){
+                System.out.println(map);
+            }
+            Map.close();
+
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
 }
+
+        /*    FileReader filereader = new FileReader(file);
+
+            int ch;
+            while ((ch = filereader.read()) != -1) {
+                System.out.print((char) ch);
+            }
+
+            filereader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+*/
